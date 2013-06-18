6ddb9086cbce1ec2dec27745cd41638b8eb8cac0
Elisabeth L <elisabeth.lang@aon.at>
2013-06-18 13:34:31 +0200

    Changes

--- shared/selfstarter/config/initializers/scaling_middleware.rb
+++ shared/selfstarter/config/initializers/scaling_middleware.rb
@@ -1,4 +1,18 @@
 class ScalingMiddleware
+	def initialize(app)
+		@app = app
+	end
+
+	def call(env)
+		log_path = File.join(Rails.root, 'log/scaling.log')
+		start = Time.now
+		status, headers, response = @app.call(env)
+		stop = Time.now - start
+		log = File.open(log_path, 'a')
+		log << "#{stop}\n"
+		log.close
+		[status, headers, response]
+	end
 end
 
-# Selfstarter::Application.config.middleware.use ScalingMiddleware
+Selfstarter::Application.config.middleware.use ScalingMiddleware
